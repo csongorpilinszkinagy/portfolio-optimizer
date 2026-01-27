c34120bdd605b7a36ec104b43f8000158bf0473a
Csongor Pilinszki-Nagy <csongor.pilinszkinagy@gmail.com>
2026-01-27 08:48:06 +0000

    Calculate maximum Sharpe ratio allocation

--- optimize.py
+++ optimize.py
@@ -1,9 +1,17 @@
 import pandas as pd
 from pypfopt.risk_models import CovarianceShrinkage
+from pypfopt import EfficientFrontier
 
 expectations = pd.read_csv("data/expectations.csv")
-prices = pd.read_csv("data/prices.csv", parse_dates=["Date"], index_col="Date")
+tickers = expectations["ticker"].to_list()
+expected_returns = expectations["CAGR"].to_list()
 
+prices = pd.read_csv("data/prices.csv", parse_dates=["Date"], index_col="Date")
 covariances = CovarianceShrinkage(prices).ledoit_wolf()
 
+ef = EfficientFrontier(expected_returns, covariances)
+ef.max_sharpe()
+weights = ef.clean_weights()
 
+for ticker, weight in weights.items():
+    print(ticker, weight)
