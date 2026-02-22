837a23c29c0c810e810d31e72ffd491bd644214e
Csongor Pilinszki-Nagy <csongor.pilinszkinagy@gmail.com>
2026-02-22 10:02:54 +0000

    Add Hungarian stocks

--- get_data.py
+++ get_data.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import yfinance as yf
 
-expectations = pd.read_csv("data/expectations.csv")
+us_expect = pd.read_csv("data/us_expect.csv")
+hu_expect = pd.read_csv("data/hu_expect.csv")
 
-tickers = expectations['ticker'].to_list()
+us_tickers = us_expect["ticker"].to_list()
+hu_tickers = hu_expect["ticker"].to_list()
 
-prices = yf.download(tickers, start="2015-01-01")["Close"]
-prices = prices.dropna(axis=1, how='all')     # Drop bad tickers entirely
-prices = prices.dropna(axis=0, how='any')     # Drop rows with missing prices
+us_prices = yf.download(us_tickers, start="2015-01-01")["Close"]
+hu_prices = yf.download(hu_tickers, start="2015-01-01")["Close"]
 
+fx = yf.download("HUFUSD=X", start="2015-01-01")["Close"]
+hu_prices = pd.concat([hu_prices, fx], axis=1, sort=False)
+hu_prices["HUFUSD=X"] = hu_prices["HUFUSD=X"].ffill()
+
+for ticker in hu_tickers:
+    hu_prices[ticker] = hu_prices[ticker] * hu_prices["HUFUSD=X"]
+
+prices = pd.concat([us_prices, hu_prices[hu_tickers]], axis=1, sort=False).sort_index()
 prices.to_csv("data/prices.csv")
--- optimize.py
+++ optimize.py
@@ -5,16 +5,21 @@ from pypfopt import EfficientFrontier
 from pypfopt.plotting import plot_efficient_frontier
 import matplotlib.pyplot as plt
 
+us_expect = pd.read_csv("data/us_expect.csv")
+us_tickers = us_expect["ticker"].to_list()
+us_expect_rets = us_expect["return"].to_list()
 
+hu_expect = pd.read_csv("data/hu_expect.csv")
+hu_tickers = hu_expect["ticker"].to_list()
+hu_expect_rets = hu_expect["return"].to_list()
 
-expectations = pd.read_csv("data/expectations.csv")
-tickers = expectations["ticker"].to_list()
-expected_returns = expectations["CAGR"].to_list()
+tickers = us_tickers + hu_tickers
+expect_rets = us_expect_rets + hu_expect_rets
 
 prices = pd.read_csv("data/prices.csv", parse_dates=["Date"], index_col="Date")
 covariances = CovarianceShrinkage(prices).ledoit_wolf()
 
-mu = expected_returns
+mu = expect_rets
 S = covariances
 
 ef = EfficientFrontier(mu, S)
@@ -28,13 +33,13 @@ rets = w.dot(mu)
 stds = np.sqrt((w.T * (S @ w.T)).sum(axis=0))
 sharpes = rets / stds
 
-ef = EfficientFrontier(expected_returns, covariances)
+ef = EfficientFrontier(mu, S)
 ef.max_sharpe()
 ret_tangent, std_tangent, _ = ef.portfolio_performance()
 weights = ef.clean_weights()
 
 for ticker, weight in weights.items():
-    print(ticker, weight)
+    print(ticker, round(weight, 2))
 
 ax.scatter(std_tangent, ret_tangent, marker="*", s=100, c="r", label="Max Sharpe")
 ax.scatter(stds, rets, marker=".", c=sharpes, cmap="viridis_r")
